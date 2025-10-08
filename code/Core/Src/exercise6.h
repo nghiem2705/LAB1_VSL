73f4e4fe08135537254d05e85a5733794ebfabf1
nghiem2705 <nghiemgithub@gmail.com>
2025-10-08 18:26:45 +0700

    update ex9

--- code/Core/Src/exercise6.h
+++ code/Core/Src/exercise6.h
@@ -10,10 +10,10 @@
 
 #include "main.h"
 
-
+void displayNumber(int num);
+void clearAllClock();
 void setNumberOnClock(int num);
 void  clearNumberOnClock(int num);
-void displayNumber(int num);
 
 
 #endif /* SRC_EXERCISE6_H_ */
