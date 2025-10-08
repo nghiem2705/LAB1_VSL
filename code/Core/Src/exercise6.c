fd44edf705ec6a2a28108f99689507298bccaebc
nghiem2705 <nghiemgithub@gmail.com>
2025-10-08 18:23:45 +0700

    update ex8

--- code/Core/Src/exercise6.c
+++ code/Core/Src/exercise6.c
@@ -182,7 +182,7 @@ void displayNumber(int num){
 	}
 }
 
-void exercise7_run(){
+void clearAllClock(){
 	HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, GPIO_PIN_RESET);
 	HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, GPIO_PIN_RESET);
 	HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, GPIO_PIN_RESET);
